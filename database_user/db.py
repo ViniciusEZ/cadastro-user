717d56715caf1c69f3986a6c14d7e2abfb627bf8
Vinicius Ezequiel <102422645+ViniciusEZ@users.noreply.github.com>
2022-09-24 20:27:23 -0300

    Updates

--- database_user/db.py
+++ database_user/db.py
@@ -1,6 +1,7 @@
 import sqlite3
 from hashlib import sha256
 from uuid import uuid4
+from database_user.validacpf import valida_cpf
 
 
 class DataBase:
@@ -23,6 +24,14 @@ class User:
     def __init__(self, name, cpf):
         self.id = str(uuid4())
         self.name = name
-        self.cpf = str(sha256(cpf.encode()).hexdigest())
+        self.cpf = cpf
 
+    @property
+    def cpf(self):
+        return self._cpf
 
+    @cpf.setter
+    def cpf(self, value):
+        while not valida_cpf(value):
+            value = input("CPF inválido. Digite novamente: ")
+        self._cpf = str(sha256(value.encode()).hexdigest())
