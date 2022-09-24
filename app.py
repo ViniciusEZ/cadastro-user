dc1d1be39fdc895b19183cb4168da1dfbf525550
Vinicius Ezequiel <102422645+ViniciusEZ@users.noreply.github.com>
2022-09-24 20:23:20 -0300

    Updates

--- app.py
+++ app.py
@@ -4,10 +4,9 @@ from database_user.validacpf import valida_cpf
 user = DataBase('usuario.db')
 
 print('BEM-VINDO. VAMOS AO CADASTRO.')
+
 name = input('Digite seu nome: ')
 cpf = input('Digite seu CPF: ')
-while not valida_cpf(cpf):
-    cpf = input('CPF inválido. Digite novamente: ')
 
 user.sql_insert(name, cpf)
 print('Cadastro feito com sucesso!')
